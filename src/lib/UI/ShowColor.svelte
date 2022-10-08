3cd8960ba483df1504c773662f59de36f480bf67
Gilles Testard <g.testard@galites.net>
2022-10-08 19:59:35 +0200

    recupSettings2

--- src/lib/UI/ShowColor.svelte
+++ src/lib/UI/ShowColor.svelte
@@ -1,19 +1,62 @@
 <script>
-    export let name = 'main_color'
-    export let value = '#2948eb'
+    import {createEventDispatcher} from 'svelte'
+    import HtmlO from "$lib/UI/EditableHtml-0.svelte"
+    import ColorPicker from '$lib/UI/elements/ColorPicker.svelte'
+
+    const dispatch = createEventDispatcher()
+
+    export let name = ''
+    export let value = ''
+
+    let isEdited = false
+
+    console.log('SettingsColors first', {value})
+
+    const editingSettings = () => isEdited = true
+    const cancelColor = () => {
+        console.log('cancelColor')
+        isEdited = false
+    }
+    const saveColor = (e) => {
+        const objColor = e.detail
+        console.log('saveColor', e.detail)
+        value = e.detail.hex
+        isEdited = false
+        dispatch('saving-color', objColor)
+    }
 </script>
-<div class="container block">
-    <div class="is-flex">
-        <div id="value" style="background-color: {value};"></div>
-        <div class="name pl-1 has-text-weight-bold" style="color: {value};">{name}</div>
-    </div>
-</div>
+{#if isEdited}
+{value}
+    <ColorPicker 
+    hex={value} 
+    label={name} 
+    on:cancel-color={cancelColor}
+    on:save-color={saveColor}
+    />
+{/if}
+
+{#if !isEdited}
+    <HtmlO 
+        label='Modifier cette couleur'
+        fct={editingSettings}
+        >
+            <div class="block">
+                <div class="is-flex">
+                    <div id="value" style="background-color: {value};"></div>
+                    <div class="name pl-1 has-text-weight-bold" style="color: {value};">{name}</div>
+                </div>
+            </div>
+        </HtmlO>
+{/if}
+
+
+
 <style lang="scss">
     $dim: 3rem;
-    .container {
-        // background-color: beige;
-        width: 20rem;
-    }
+    // .container {
+    //     // background-color: beige;
+    //     width: 20rem;
+    // }
     .is-flex {
         // background-color: coral;
         height: $dim;
--- src/lib/components/settings/SettingsColors.svelte
+++ src/lib/components/settings/SettingsColors.svelte
@@ -2,19 +2,21 @@
      import {createEventDispatcher} from 'svelte'
     import f from '$lib/helpers/scripts'
     import TextInput from "$lib/UI/TextInput.svelte"
-    import HtmlO from "$lib/UI/EditableHtml-0.svelte"
-    import ColorPicker from '$lib/UI/elements/ColorPicker.svelte'
+
     import ShowColor from '$lib/UI/ShowColor.svelte'
-    import Buttons from '$lib/UI/ButtonsCancelConfirm.svelte'
+
 
     const dispatch = createEventDispatcher()
 
     export let itemToEdit = null   
 
-    // let full_name = ''
-    // let avatar = ''
-    // let job = ''
-    // let speech_about = ''
+    let main_color_1 = ''
+    let main_color_2 = ''
+    let main_color_3 = ''
+    let hove_link_color = ''
+    let hover_bg_link_color = ''
+    let bg_color = ''
+    let bg_banner = ''
 
     let itemBup = {}
 
@@ -24,68 +26,88 @@
         itemBup = {...itemToEdit}
         keys = Object.keys(itemToEdit)
         console.log('kyes', {keys})
-        // full_name = itemToEdit.full_name
-        // avatar = itemToEdit.avatar
-        // job = itemToEdit.job
-        // speech_about = itemToEdit.speech_about
+        main_color_1 = itemToEdit.main_color_1
+        main_color_2 = itemToEdit.main_color_2
+        main_color_3 = itemToEdit.main_color_3
+        hove_link_color = itemToEdit.hove_link_color
+        hover_bg_link_color = itemToEdit.hover_bg_link_color
+        bg_color = itemToEdit.bg_color
+        bg_banner = itemToEdit.bg_banner
     }
 
-    let isEdited = false
-    let disableConfirm = true; 
-    let disableCancel = true; 
-
-    const editingSettings = () => isEdited = true
-
-    const cancelUpdates = () => {
-        console.log('cancelUpdates')
-        isEdited = false
-    }
-    const saveUpdates = () => {
+    const saveUpdates = (e) => {
+        const {label, hex} = e.detail
         const itemUpdated = {...itemBup}
-        console.log('saveUpdates')
-        // if (itemBup.full_name !== full_name) {
-        //     console.log('saveUpdates', {full_name})
-        //     itemUpdated.full_name = full_name
-        // }
-        console.log('saveUpdates Manager',{itemUpdated})  
+        console.log('saveUpdates', e.detail)
+
+        if (label === 'main_color_1') {
+            if (itemBup.main_color_1 !== hex) {
+                itemUpdated.main_color_1 = hex
+                console.log('saveUpdates', itemUpdated.main_color_1)
+            }
+        }
+
+        if (label === 'main_color_2') {
+            if (itemBup.main_color_2 !== hex) {
+                itemUpdated.main_color_2 = hex
+                console.log('saveUpdates', itemUpdated.main_color_2)
+            }
+        }
+
+        if (label === 'main_color_3') {
+            if (itemBup.main_color_3 !== hex) {
+                itemUpdated.main_color_3 = hex
+                console.log('saveUpdates', itemUpdated.main_color_3)
+            }
+        }
+
+        if (label === 'hove_link_color') {
+            if (itemBup.hove_link_color !== hex) {
+                itemUpdated.hove_link_color = hex
+                console.log('saveUpdates', itemUpdated.hove_link_color)
+            }
+        }
+
+        if (label === 'hover_bg_link_color') {
+            if (itemBup.hover_bg_link_color !== hex) {
+                itemUpdated.hover_bg_link_color = hex
+                console.log('saveUpdates', itemUpdated.hover_bg_link_color)
+            }
+        }
+
+        if (label === 'bg_color') {
+            if (itemBup.bg_color !== hex) {
+                itemUpdated.bg_color = hex
+                console.log('saveUpdates', itemUpdated.bg_color)
+            }
+        }
+
+        if (label === 'bg_banner') {
+            if (itemBup.bg_banner !== hex) {
+                itemUpdated.bg_banner = hex
+                console.log('saveUpdates', itemUpdated.bg_banner)
+            }
+        }
+
+        console.log('saveUpdates colors',{itemUpdated})  
         dispatch('save-item-updated', itemUpdated)
-        isEdited = false
     }
+
+    // let color
+    // console.log('Settings Colors', {color})
 </script>
 
-{#if isEdited}
-    <!-- <TextInput
-    id="full-name"
-    label="Nom complet"
-    type="text"
-    valid={full_nameValid}
-    validityMessage="Entrez votre prénom et votre nom de famille"
-    controlType="input"
-    value={full_name}
-    on:input={event => (full_name = event.target.value)} 
-    /> -->
-    Colors edit
-    <ColorPicker />
-    <Buttons 
-    cancelText='Abandonner'
-    saveText='Enregistrer'
-    {disableConfirm}
-    {disableCancel}
-    on:button-canceling={cancelUpdates}
-    on:button-saving={saveUpdates}
-    />
-{/if}
-{#if !isEdited}
-    <HtmlO 
-    label={`Charte graphique`}
-    fct={editingSettings}
-    >
-        {#if keys?.length > 0}
-            <div>
-             {#each keys as key}
-                <ShowColor name={key} value={itemToEdit[key]} /> 
-             {/each}
-            </div>
-        {/if}
-    </HtmlO>
-{/if}
+
+    <p class="label">Charte graphique</p>
+
+    {#if keys?.length > 0}
+        <div>
+        {#each keys as key}
+            <ShowColor 
+            name={key} 
+            value={itemToEdit[key]} 
+            on:saving-color={saveUpdates}
+            />
+        {/each}
+        </div>
+    {/if}
